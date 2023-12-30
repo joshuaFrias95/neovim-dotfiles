9f058d99b5f01f54152e146e9ab38c404472d22e
Joshua Frías <metatron1495@gmail.com>
2023-12-30 02:55:09 -0600

    Add: funcionalidades git

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,11 @@
+return {
+  "NeogitOrg/neogit",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "sindrets/diffview.nvim",
+    "ibhagwan/fzf-lua",
+  },
+  config = true,
+}
