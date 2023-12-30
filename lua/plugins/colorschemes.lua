8105a58305dd46c47c76b6f7880d042da4725542
Joshua Frías <metatron1495@gmail.com>
2023-12-30 01:21:20 -0600

    Add: Colorschemes

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,23 @@
+return {
+  "AhmedAbdulrahman/aylin.vim",
+  "sainnhe/everforest",
+  "ellisonleao/gruvbox.nvim",
+  "rebelot/kanagawa.nvim",
+  {
+    "EdenEast/nightfox.nvim",
+    name = "nightfox",
+    lazy = false,
+    opts = { flavor = "carbonfox" },
+  },
+  {
+    "hachy/eva01.vim",
+    lazy = false,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "eva01-LCL",
+    },
+  },
+}
